252ff1e025ce52352c23476f389eabee36b356ed
antessio <antessio7@gmail.com>
2025-02-16 11:02:16 +0100

    add phoenix

--- lib/personal_finance.ex
+++ lib/personal_finance.ex
@@ -1,18 +1,9 @@
 defmodule PersonalFinance do
   @moduledoc """
-  Documentation for `PersonalFinance`.
+  PersonalFinance keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PersonalFinance.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
