e96665796fbf2af4455761408cd5fcd969222a7c
antessio <antessio7@gmail.com>
2025-03-02 12:51:06 +0100

    style refactoring

--- lib/personal_finance_web/components/core_components.ex
+++ lib/personal_finance_web/components/core_components.ex
@@ -231,8 +231,8 @@ defmodule PersonalFinanceWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        "phx-submit-loading:opacity-75",
+        "rounded-md bg-blue-600 px-6 py-3 text-base font-semibold text-white hover:bg-blue-500",
         @class
       ]}
       {@rest}
@@ -429,7 +429,7 @@ defmodule PersonalFinanceWeb.CoreComponents do
     ~H"""
     <header class={[@actions != [] && "flex items-center justify-between gap-6", @class]}>
       <div>
-        <h1 class="text-lg font-semibold leading-8 text-zinc-800">
+        <h1 class="text-lg font-semibold leading-8 text-blue-600">
           {render_slot(@inner_block)}
         </h1>
         <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
@@ -473,48 +473,50 @@ defmodule PersonalFinanceWeb.CoreComponents do
       end
 
     ~H"""
-    <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0">
-      <table class="w-[40rem] mt-11 sm:w-full">
-        <thead class="text-sm text-left leading-6 text-zinc-500">
-          <tr>
-            <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal">{col[:label]}</th>
-            <th :if={@action != []} class="relative p-0 pb-4">
-              <span class="sr-only">{gettext("Actions")}</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody
-          id={@id}
-          phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
-        >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
-            <td
-              :for={{col, i} <- Enum.with_index(@col)}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  {render_slot(col, @row_item.(row))}
-                </span>
-              </div>
-            </td>
-            <td :if={@action != []} class="relative w-14 p-0">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  {render_slot(action, @row_item.(row))}
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="flex justify-center">
+      <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0">
+        <table class="w-[40rem] mt-11 sm:w-full bg-white rounded-lg shadow-sm">
+          <thead class="text-sm text-left leading-6 text-zinc-500">
+            <tr>
+              <th :for={col <- @col} class="px-5 py-2 pb-4 pr-6 font-normal">{col[:label]}</th>
+              <th :if={@action != []} class="relative p-0 pb-4">
+                <span class="sr-only">{gettext("Actions")}</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody
+            id={@id}
+            phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
+            class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
+          >
+            <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group">
+              <td
+                :for={{col, i} <- Enum.with_index(@col)}
+                phx-click={@row_click && @row_click.(row)}
+                class={["relative p-0 px-5", @row_click && "hover:cursor-pointer"]}
+              >
+                <div class="block py-4 pr-6">
+                  <span class="absolute -inset-y-px right-0 -left-4  sm:rounded-l-xl" />
+                  <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
+                    {render_slot(col, @row_item.(row))}
+                  </span>
+                </div>
+              </td>
+              <td :if={@action != []} class="relative w-14 px-5">
+                <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
+                  <span class="absolute -inset-y-px -right-4 left-0  sm:rounded-r-xl" />
+                  <span
+                    :for={action <- @action}
+                    class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
+                  >
+                    {render_slot(action, @row_item.(row))}
+                  </span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     """
   end
