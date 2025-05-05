0190add1eeaa7206d637ee2f9e50f8955a0aec93
Antonio D'Alessio <antessio7@gmail.com>
2025-05-05 11:39:37 +0200

    widiba test

--- java-backend/domain/src/main/java/antessio/personalfinance/domain/service/transactionparser/WidibaTransactionParser.java
+++ java-backend/domain/src/main/java/antessio/personalfinance/domain/service/transactionparser/WidibaTransactionParser.java
@@ -1,11 +1,26 @@
 package antessio.personalfinance.domain.service.transactionparser;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import antessio.personalfinance.domain.dto.CreateTransactionDTO;
 import antessio.personalfinance.domain.model.TransactionImport;
 
-public class WidibaTransactionParser implements TransactionParser{
+public class WidibaTransactionParser implements TransactionParser {
 
     @Override
     public boolean canParse(TransactionImport transactionImport) {
@@ -14,7 +29,66 @@ public class WidibaTransactionParser implements TransactionParser{
 
     @Override
     public List<CreateTransactionDTO> parse(TransactionImport transactionImport) {
-        return List.of();
+        try {
+            return processAccount(transactionImport.getFilePath(), transactionImport.getUserOwner());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final int SKIP_ROWS = 19;
+    private static final Pattern DATE_TIME_REGEX = Pattern.compile("Data\\s(\\d{2}/\\d{2}/\\d{2})\\sOra\\s(\\d{2}\\.\\d{2})");
+
+    private List<CreateTransactionDTO> processAccount(String filePath, String userOwner) throws IOException {
+        List<CreateTransactionDTO> transactions = new ArrayList<>();
+        try (
+                FileInputStream fis = new FileInputStream(filePath);
+                Workbook workbook = new XSSFWorkbook(fis)
+        ) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+            int rowCount = sheet.getLastRowNum();
+
+            for (int i = SKIP_ROWS; i <= rowCount; i++) {
+                Row row = sheet.getRow(i);
+                parseRow(row, userOwner)
+                        .ifPresent(transactions::add);
+            }
+        }
+        return transactions;
+    }
+
+    private Optional<CreateTransactionDTO> parseRow(Row row, String userOwner) {
+        try {
+            LocalDateTime settlementDate = row.getCell(1).getLocalDateTimeCellValue();
+            String reason = row.getCell(3).getStringCellValue();
+            String description = row.getCell(4).getStringCellValue();
+            double amount = row.getCell(6).getNumericCellValue();
+            if (settlementDate == null || reason == null || reason.isEmpty() || description == null || description.isEmpty() || amount == 0) {
+                return Optional.empty();
+
+            }
+            LocalDate transactionDate = extractDateTime(description).orElse(settlementDate.toLocalDate());
+
+            return Optional.of(new CreateTransactionDTO(userOwner, transactionDate, BigDecimal.valueOf(amount), description + " " + reason, "widiba"));
+        } catch (Exception e) {
+            return Optional.empty(); // Ignora righe non valide
+        }
+    }
+
+    private java.util.Optional<LocalDate> extractDateTime(String description) {
+        if (description == null) {
+            return java.util.Optional.empty();
+        }
+        Matcher matcher = DATE_TIME_REGEX.matcher(description);
+        if (matcher.find()) {
+            String date = matcher.group(1);
+            String time = matcher.group(2).replace(".", ":");
+            String dateTimeString = date + " " + time;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+            return java.util.Optional.of(LocalDate.parse(dateTimeString, formatter));
+        }
+        return java.util.Optional.empty();
     }
 
 }
